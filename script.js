1029e089a9037dcd8b3612610a555f1a1f1e6c8a
Isabel Toledo <isabeltoledo9@gmail.com>
2021-04-16 16:18:09 -0600

    added variables

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//first need to delcare variables
 
+//search variables
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+
+//accessing stats variables 
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var city="";
+
+var sCity=[];
 
 
